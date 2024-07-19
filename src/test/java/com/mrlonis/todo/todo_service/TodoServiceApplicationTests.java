5ae07c8ca3beed5be54bcc04fecd40eb5ace864a
Matthew Lonis <mattlonis@gmail.com>
2024-07-18 22:46:59 -0400

    Initial Commit

---
+++ src/test/java/com/mrlonis/todo/todo_service/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlonis.todo.todo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
